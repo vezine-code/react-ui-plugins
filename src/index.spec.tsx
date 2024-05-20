91c0007c419b15022ceb170af1dc14ed3c9e185a
Wayne <wayne@iplayulisten.com>
2024-05-19 23:08:38 -0400

    update esm config

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Mock, describe, expect, it, vi } from "vitest";
